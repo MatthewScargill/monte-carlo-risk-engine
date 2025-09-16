3c7f94d91e6c4175a84dcdb92997376988f9d2a1
MatthewScargill <matthewscargilluni@gmail.com>
2025-09-16 10:34:47 +0100

    fixed venv and requirements.txt, added vis.py ready for visualisation functions

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Monte Carlo Risk Engine
+
+
+source .venv/bin/activate 
+putting this here because i am bored of googling how to get into the venv
---
+++ src/vis.py
@@ -0,0 +1,7 @@
+import matplotlib
+
+# comparative and illustrative visualisations
+
+# 1) comparing performace with different additions against hsitorical data (add backtesting later)
+# 2) showing all the simulated paths (and colour coding them for risk)
+# 3) future projections for various stocks (maybe have them fade into red as the time steps get longer and longer to show possible inaccuracy)
